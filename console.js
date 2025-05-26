c10db4d1564434f0272c5162fcec2c8bb5501da2
Ashikur <35762199+AshikurRahman-Peters@users.noreply.github.com>
2025-05-26 10:19:33 +0600

    animation js

--- console.js
+++ console.js
@@ -1,2 +1,41 @@
-console.log("Hello world");
-console.log("Hello world");
+const wrapper = document.getElementById("wrapper");
+
+for (let i = 1; i < 51; i++) {
+  let div = document.createElement("div");
+  animeCircle = wrapper.appendChild(div);
+  animeCircle.classList.add("anime-circle");
+}
+
+let circles = document.querySelectorAll(".anime-circle");
+
+let randomAnimation = anime({
+  targets: circles,
+  background: () => {
+    let hue = anime.random(0, 360);
+    let saturation = 60;
+    let lumonisity = 70;
+    let hslValue = "hsl(" + hue + "," + saturation + "%," + lumonisity + "%)";
+    return hslValue;
+  },
+  borderRadius: ()=> {
+    return anime.random(25,50);
+  },
+  translateX: ()=> {
+    return anime.random(-80,80) + "vh";
+  },
+  translateY: ()=> {
+    return anime.random(-80,80) + "vh" 
+  },
+  scale: ()=> {
+    return anime.random(0.45,1.75);
+  },
+  duration: ()=> {
+    return anime.random(250,1500);
+  },
+  delay: ()=> {
+    return anime.random(500,1000);
+  },
+  loop: true,
+  direction: "alternate",
+  easing: "easeOutExpo"
+});
